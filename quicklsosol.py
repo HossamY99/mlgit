fa00dd80ad7171783574b65fec4897a685ac5588
Hossam Younes <hossamyounes1148@egmail.com>
2021-04-21 15:01:48 +0300

    updatedforapp

--- quicklsosol.py
+++ quicklsosol.py
@@ -12,7 +12,7 @@ from flask import request, jsonify
 from datetime import timedelta
 import pickle
 import json
-
+import other
 import pandas as pd
 import numpy as np
 import math
@@ -179,106 +179,7 @@ def main():
         return phrase
     
         
-        
-    def makecor(df,corr,per):
-        catstr=df['category_name'][0]
-        b=(df['brand_name'][0]).lower()
-        con=int(df['item_condition_id'][0])
-        sh=int(df['shipping'][0])
-        namestr=(df['name'][0]).lower()
-        desstr=(df['item_description'][0]).lower()
-            
-        if sh==1:
-            #per=per+0.1
-            corr=corr+5
-        if con==5:
-            per=per-0.9
-        elif con==4:
-            per=per-0.7
-        elif con==3:
-            if (" & Smartphones" in catstr):
-                corr=100
-                per=per+2
-            per=per-0.5
-        elif con==2:
-            if (" & Smartphones" in catstr):
-                corr=100
-                per=per+2 
-            else:
-                per=per-0.3
-        elif con==1:
-             if (" & Smartphones" in catstr):
-                corr=300
-                per=per+3    
-             else:
-                per=per+0.5
-            
-        if (len(desstr)>200):
-            per=per-0.8
-          
-        if (b=="apple"):
-            per=per+1
-        
-        if (" & Smartphones" in catstr):
-            #corr=100
-            per=per+0.5
-            
-        if ("Shoes" in catstr):
-            #corr=100
-            per=per-0.4
-        
-        elif ("/Headphones" in catstr):
-          #  corr=50
-            per=per+1.5
-            
-        #print(int('iphone12')) 
-        if ("new" in namestr):
-            #corr=100
-            per=per+0.2
-        
-        if ("iphone x" in namestr):
-            corr=corr+100
-            #per=per+0.5
-            
-        if ("7 128gb"in namestr):
-            corr=corr-300
-            #per=per+0.5
-            
-         
-        for s in namestr.split():
-            if s.isdigit():
-                #corr=corr+int(s)*10
-                per=per+int(s)/10
-                print(per)
-        
-        
-       
-        if (("good" or "great" ) in desstr):
-            #corr=1
-            per=per+0.2
-      #      per=per+0.1
-            
-        if (("new") in desstr):
-            #corr=1
-            per=per+0.3
-       #     per=per+0.3
-        
-        
-        if (("broken" or "damaged" or "crack") in desstr):
-            #corr=1
-            per=per-0.9
-            
-        if (("needs") in desstr):
-            #corr=1
-            per=per-0.9
-        
-        if (("except") in desstr):
-            #corr=1
-            per=per-0.5
-                
-        
-        return(corr,per)
-        
+  
 
     
     
@@ -539,7 +440,7 @@ def main():
     
     corr=0
     per=1
-    (corr,per)=makecor(df,corr,per)
+    (corr,per)=other.makecor(df,corr,per)
     
 
 
@@ -548,7 +449,8 @@ def main():
     finalres=per*res+corr
     finalres = np.round(finalres, 1)
     finalres=str(finalres)
-    print(finalres)
+    print("finalres: ",finalres)
+    #printstuff()
     return jsonify({'greetings': finalres}) 
     
     
